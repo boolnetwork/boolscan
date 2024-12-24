d1b31254d2307ff1926f6023ce74ad65283fda5e
wangjupenng <w875050772@sina.com>
2024-12-25 00:13:12 +0800

    icon create

--- deploy/scripts/entrypoint.sh
+++ deploy/scripts/entrypoint.sh
@@ -50,13 +50,13 @@ else
 fi
 
 # Generate favicons bundle
-# ./favicon_generator.sh
-# if [ $? -ne 0 ]; then
-#   echo "👎 Unable to generate favicons bundle."
-# else
-#   echo "👍 Favicons bundle successfully generated."
-# fi
-# echo
+./favicon_generator.sh
+if [ $? -ne 0 ]; then
+  echo "👎 Unable to generate favicons bundle."
+else
+  echo "👍 Favicons bundle successfully generated."
+fi
+echo
 
 # Create envs.js file with run-time environment variables for the client app
 ./make_envs_script.sh
