3d7c178055deda6f6ffccb42180282e90a48d311
wangjupenng <w875050772@sina.com>
2024-12-25 01:16:53 +0800

    icon bash

--- deploy/scripts/favicon_generator.sh
+++ deploy/scripts/favicon_generator.sh
@@ -2,7 +2,7 @@
 
 master_url="${FAVICON_MASTER_URL:-$NEXT_PUBLIC_NETWORK_ICON}"
 export MASTER_URL="$master_url"
-
+echo "icon url is $MASTER_URL"
 cd ./deploy/tools/favicon-generator
 yarn install --frozen-lockfile
 node "$(pwd)/index.js"
